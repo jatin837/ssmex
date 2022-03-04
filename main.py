a54e83e01aad6fce07cb9e36814a586fd75b506e
Jatin Pandey <pandeyjatin47@gmail.com>
2022-03-04 13:20:57 +0530

    diverging output
    incorrect

--- main.py
+++ main.py
@@ -13,18 +13,23 @@ B = np.array([0,1/m])
 C = np.array([1, 0])
 D = 0
 
-F = 1
+def F(t):
+    if t<1:
+        return 0
+    return 1
 
-t = np.array(range(100))
+t = np.arange(100)
 Y = []
-for time in t:
-    X0 = np.array([x0, v0])
-    X = np.matmul(A, X0.T) + np.dot(B, F*np.ones(2).T)
-    x0, v0 = X
 
-    y = np.matmul(np.array([1, 0]), X.T) 
+X0 = np.array([x0, v0])
+
+for time in t:
+    Xdot = np.matmul(A, X0.T) + np.matmul(B, F(time)*np.ones(2).T)
+    X0 = X0 + Xdot
+
+    y = np.matmul(np.array([1, 0]), X0.T) 
     Y.append(y)
-    print(X, y)
+    print(time, y)
 
 plt.plot(t, Y)
 plt.show()
